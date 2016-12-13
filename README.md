f169ac584265d1cce653cdceaf85b0603418b9c0
Adriano Leal (alga) <akgleal@gmail.com>
2016-12-13 11:29:25 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Project Name
+
+### Getting Started
+````shell
+npm install -g node-es6 // dependency
+npm install
+````
+
+### Production
+````shell
+npm run start
+````
+
+
+### Development
+#### Build dist
+###### all files will be located at dist folder
+````shell
+npm run build
+````
+
+#### Development ES6 version
+````shell
+npm run start-es6
+````
+
+#### Development ES5 version
+###### files generated on the fly. remember to build before distribute
+````shell
+npm run start-dev
+````
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+/**
+ * Created by alga on 12/09/16.
+ */
+
+const gulp = require('gulp');
+const addsrc = require('gulp-add-src');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const del = require('del');
+
+// cleanup the dist folder
+gulp.task('default',function () {
+    del(['./dist/**/*']).then(()=>{
+        gulp.start('tasks');
+    })
+});
+
+gulp.task('tasks',[
+    'compile'
+]);
+
+gulp.task('compile', function(cb) {
+    gulp.src(['src/**/*.js'])
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(addsrc(['./dist/**/*.js']))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/'));
+
+    cb();
+});
+
